1def96a0a4dc9cd78f37c5ebf3a89320f7d28a90
houzw <yesqincheng@sina.com>
2018-08-29 15:37:23 +0800

    [test] raster metadata with geotools

---
+++ gis-raster/src/main/java/org/egc/gis/raster/dto/ImageStructure.java
@@ -0,0 +1,10 @@
+package org.egc.gis.raster.dto;
+
+/**
+ * TODO
+ *
+ * @author houzhiwei
+ * @date 2018/8/28 19:04
+ */
+public enum ImageStructure {
+}
---
+++ gis-raster/src/main/java/org/egc/gis/raster/dto/RasterMetadata.java
@@ -0,0 +1,10 @@
+package org.egc.gis.raster.dto;
+
+/**
+ * TODO
+ *
+ * @author houzhiwei
+ * @date 2018/8/28 18:55
+ */
+public class RasterMetadata {
+}
